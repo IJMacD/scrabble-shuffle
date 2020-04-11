c3ce102b3a91855f42167cacf33d054f263fb344
Iain MacDonald <IJMacD@gmail.com>
2020-04-11 17:55:09 +0800

    Updates

---
+++ precache-manifest.a4c466b328d21140f17cbb30a060f1db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abf7dadc764631627ce44772091ba06",
+    "url": "/scrabble-shuffle/index.html"
+  },
+  {
+    "revision": "9cee637aca52951e6181",
+    "url": "/scrabble-shuffle/static/css/main.4122cfe1.chunk.css"
+  },
+  {
+    "revision": "d2016091abb65f368021",
+    "url": "/scrabble-shuffle/static/js/2.5f8a5db2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scrabble-shuffle/static/js/2.5f8a5db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cee637aca52951e6181",
+    "url": "/scrabble-shuffle/static/js/main.a9c659f6.chunk.js"
+  },
+  {
+    "revision": "3b4873048c36f6984a97",
+    "url": "/scrabble-shuffle/static/js/runtime-main.1ed9cec0.js"
+  }
+]);
